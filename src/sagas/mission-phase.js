4d6a0bc82fe3fe75b79b66043adf849de48a9e98
bodiddlie <bodiddlie@gmail.com>
2017-07-05 16:53:21 -0700

    got sagas for mission phase started

--- src/sagas/mission-phase.js
+++ src/sagas/mission-phase.js
@@ -1,7 +1,31 @@
-import { select } from 'redux-saga/effects';
+import { select, put } from 'redux-saga/effects';
+
+import { depleteCrystals, setCrystals } from '../dux/crystals';
+import { failMission, drawMissionCard } from '../dux/missions';
+import { drawEvent, activateEvent } from '../dux/events';
 
 export function* depleteCrystalReserves() {
   const activeMission = yield select(selectors.activeMission);
+  if (!activeMission) return;
+
+  yield put(depleteCrystals());
+  const crystals = yield select(selectors.crystals);
+
+  if (crystals > 0) return;
+
+  yield put(failMission());
+  yield put(drawEvent());
+  const drawn = yield select(selectors.drawnEvent);
+  yield put(activateEvent(drawn));
+}
+
+export function* refreshMission() {
+  const activeMission = yield select(selectors.activeMission);
+  if (!!activeMission) return;
+
+  yield put(drawMissionCard());
+  const difficulty = yield select(selectors.difficulty);
+  yield put(setCrystals(10 - (difficulty - 1)));
 }
 
 //-----------------------
@@ -9,4 +33,7 @@ export function* depleteCrystalReserves() {
 //-----------------------
 export const selectors = {
   activeMission: state => state.missions.activeMission,
+  crystals: state => state.crystals,
+  drawnEvent: state => state.events.drawnEvent,
+  difficulty: state => state.difficulty,
 };
--- src/sagas/tests/mission-phase.test.js
+++ src/sagas/tests/mission-phase.test.js
@@ -1,18 +1,82 @@
 import { put, select } from 'redux-saga/effects';
 
 import { depleteCrystalReserves, refreshMission, selectors } from '../mission-phase';
+import { depleteCrystals, setCrystals } from '../../dux/crystals';
+import { failMission, drawMissionCard } from '../../dux/missions';
+import { drawEvent, activateEvent } from '../../dux/events';
 
 describe('Mission Phase Sagas', () => {
   describe('Deplete Crystal Reserves Saga', () => {
-    it('it will do nothing if no mission is in play', () => {
-      const gen = depleteCrystalReserves();
-      let result = gen.next();
+    let gen;
+    let result;
+
+    beforeEach(() => {
+      gen = depleteCrystalReserves();
+      result = gen.next();
       expect(result.value).toEqual(select(selectors.activeMission));
+    });
+
+    it('will do nothing if no mission is in play', () => {
       result = gen.next(null);
       expect(result.done).toBeTruthy();
     });
+
+    it('will deplete the crystals if a mission is active', () => {
+      result = gen.next('test');
+      expect(result.value).toEqual(put(depleteCrystals()));
+    });
+
+    it('will be done if there are still crystals left after deplete', () => {
+      result = gen.next('test');
+      expect(result.value).toEqual(put(depleteCrystals()));
+      result = gen.next();
+      expect(result.value).toEqual(select(selectors.crystals));
+      result = gen.next(5);
+      expect(result.done).toBeTruthy();
+    });
+
+    it('will fail the mission and draw an event card if crystals reach 0', () => {
+      result = gen.next('test');
+      expect(result.value).toEqual(put(depleteCrystals()));
+      result = gen.next();
+      expect(result.value).toEqual(select(selectors.crystals));
+      result = gen.next(0);
+      expect(result.value).toEqual(put(failMission()));
+      result = gen.next();
+      expect(result.value).toEqual(put(drawEvent()));
+      result = gen.next();
+      expect(result.value).toEqual(select(selectors.drawnEvent));
+      result = gen.next('test');
+      expect(result.value).toEqual(put(activateEvent('test')));
+      result = gen.next();
+      expect(result.done).toBeTruthy();
+    });
   });
+
   describe('Refresh Mission Saga', () => {
-    it('do nothing if a mission card is already in play', () => {});
+    let gen;
+    let result;
+
+    beforeEach(() => {
+      gen = refreshMission();
+      result = gen.next();
+      expect(result.value).toEqual(select(selectors.activeMission));
+    });
+
+    it('does nothing if a mission card is already in play', () => {
+      result = gen.next('test');
+      expect(result.done).toBeTruthy();
+    });
+
+    it('draw an new mission, activates it, and resets crystals if no mission is active', () => {
+      result = gen.next(null);
+      expect(result.value).toEqual(put(drawMissionCard()));
+      result = gen.next();
+      expect(result.value).toEqual(select(selectors.difficulty));
+      result = gen.next(2);
+      expect(result.value).toEqual(put(setCrystals(9)));
+      result = gen.next();
+      expect(result.done).toBeTruthy();
+    });
   });
 });
